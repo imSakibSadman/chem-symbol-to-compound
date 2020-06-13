79f773928d7d0e9206f9b6e29b21af14ddb3a7c4
Sadman Sakib <sakibsadman@gmail.com>
2020-06-13 20:52:30 +0600

    final

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
 
-symbol_to_name = {
-    "o2": "oxygen",
-    "co2": "carbon dioxide",
-    "cl2": "chlorine",
-    "znso4": "zinc sulfate",
-    "cuso4": "copper sulfate",
-    "s04": "sulfate",
-    "k2cr2o7": "potassium dichromate",
-}
+import csv
 
-name_to_symbol = {value: key for key, value in symbol_to_name.items()}
+database = {}
+with open('./chem-symbol-to-name/database.csv', 'r') as database_file:
+    readerx = csv.reader(database_file)
+    for row in readerx:
+        database[row[0]] = row[1]
+
+exeptions = []
+with open('./chem-symbol-to-name/exeptions.csv', 'r') as exeptions_file:
+    readery = csv.reader(exeptions_file)
+    l = list(readery)
+    for sublist in l:
+        for item in sublist:
+            exeptions.append(item)
 
 def converter():
-    prompt = input("Enter a symbol of a chemical compound OR TYPE q TO QUIT: ")
-    
-    for symbol in symbol_to_name:
-        if prompt in symbol_to_name:
-            to = "name"
-        elif prompt == "q":
+    prompt = input("Enter a symbol or a name of a chemical compound OR ENTER q TO QUIT: ")
+
+    for expetion in exeptions:
+        if prompt in exeptions:
+            prompt = prompt.capitalize()
+
+    for symbol in database:
+        if prompt in database or prompt == "q":
             break
         else:
-            for name in name_to_symbol:
-                if prompt in name_to_symbol:
-                    to = "symbol"
-                else:
-                    print("Not found in our database")
-                    return True
+            print("Not found in our database")
+            return True
 
     if prompt == "q":
         print("Thank You!")
         return False
-    elif to == "name":
-        print(symbol_to_name[prompt])
-    elif to == "symbol":
-        print(name_to_symbol[prompt])
+
+    print(database[prompt])
+
 
 while converter() is not False:
         converter()
-        
-
+    
