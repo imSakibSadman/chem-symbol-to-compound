bfe85a265e17af57c753d3c11cdc563c5cd43500
Sadman Sakib <sakibsadman@gmail.com>
2020-06-14 21:28:47 +0600

    back to previuos version

--- main.py
+++ main.py
@@ -17,26 +17,27 @@ with open(os.path.abspath(os.path.dirname(sys.argv[0])) + '/exeptions.csv', 'r')
         for item in sublist:
             exeptions.append(item)
 
-quiter = False
-
-while quiter is not True: 
+def converter():
     prompt = input("Enter a symbol or a name OR ENTER q TO QUIT: ")
     alt_prompt = prompt.lower()
 
-    for expetion in exeptions:
+    for expetion in exeptions: #Checks if it is a name or symbol. If it is a name, makes it capital.
         if alt_prompt in exeptions:
             prompt = alt_prompt.capitalize()
 
-    for symbol in database:
+    for symbol in database: #Checks if the input is in the database or not. If it is not, prints it.
         if prompt in database or prompt == "q":
             break
         else:
-            print("Not found in our database")
-            quiter = False
+            print("Not found in our database!")
+            return True
 
-    if prompt == "q":
+    if prompt == "q": #Checks if the input is 'q'. If it is, quits the program.
         print("Thank You!")
-        quiter = True
-    else:
-        print(database[prompt])
-    
+        return False
+
+    print(database[prompt])
+
+
+while converter() is not False:
+        converter()
