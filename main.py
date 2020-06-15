0cd94db0f3261af54531424ea8161b848b6840b0
Sadman Sakib <sakibsadman@gmail.com>
2020-06-16 04:55:48 +0600

    added subscript and superscript

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import csv
 import os
 import sys
 
-database = {}
+database = {} # Reading database
 with open(os.path.abspath(os.path.dirname(sys.argv[0])) + '/database.csv', 'r') as database_file:
     readerx = csv.reader(database_file)
     for row in readerx:
         database[row[0]] = row[1]
 
-exeptions = []
+database_file.close()
+
+exeptions = [] # Reading exeptions
 with open(os.path.abspath(os.path.dirname(sys.argv[0])) + '/exeptions.csv', 'r') as exeptions_file:
     readery = csv.reader(exeptions_file)
     l = list(readery)
@@ -17,26 +19,32 @@ with open(os.path.abspath(os.path.dirname(sys.argv[0])) + '/exeptions.csv', 'r')
         for item in sublist:
             exeptions.append(item)
 
+exeptions_file.close()
+
+SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+inv_SUB = str.maketrans("₀₁₂₃₄₅₆₇₈₉", "0123456789")
+
 def converter():
-    prompt = input("Enter a symbol or a name OR ENTER q TO QUIT: ")
+    prompt = input("Enter a symbol or a name OR ENTER q TO QUIT: ").translate(inv_SUB)
     alt_prompt = prompt.lower()
 
-    for expetion in exeptions: #Checks if it is a name or symbol. If it is a name, makes it capital.
+    for expetion in exeptions: # Checks if it is a name or symbol. If it is a name, makes it capital.
         if alt_prompt in exeptions:
             prompt = alt_prompt.capitalize()
 
-    for symbol in database: #Checks if the input is in the database or not. If it is not, prints it.
+    for symbol in database: # Checks if the input is in the database or not. If it is not, prints it.
         if prompt in database or prompt == "q":
             break
         else:
             print("Not found in our database!")
             return True
 
-    if prompt == "q": #Checks if the input is 'q'. If it is, quits the program.
+    if prompt == "q": # Checks if the input is 'q'. If it is, quits the program.
         print("Thank You!")
         return False
-
-    print(database[prompt])
+ 
+    result = database[prompt].translate(SUB)
+    print(result)
 
 
 while converter() is not False:
